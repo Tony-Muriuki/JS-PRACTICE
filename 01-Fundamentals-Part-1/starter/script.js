9e7da412df39e871ac55b1d5c76d3e7e08ab9eaf
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-08 15:15:25 +0300

    If/Else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -105,3 +105,14 @@
 // console.log(`
 //     multiple
 //     lines`);
+
+// TAKING DECISIONS IF/ELSE STATEMENTS
+
+let birthYear = 1998;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);

ad5804874896eee1a841eda08a2f1709375fd32b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-08 04:33:01 +0300

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,47 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is Fun!");
+// let js = "amazing";
+// // if (js === "amazing") alert("Javascript is Fun!");
 
-/*Math Operation*/
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// // Value And Variables
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Jonas"; //Declaration of a Variable firstName.
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.13415;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+
+// console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// TYPEOF Operator
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+console.log(typeof null);
+
+// Dynamic Typing
+javascriptIsFun = "Yes";
+console.log(typeof javascriptIsFun);
+
+// Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;

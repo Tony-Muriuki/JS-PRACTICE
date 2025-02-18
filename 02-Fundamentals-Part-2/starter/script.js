160fdd8cc6bd0cebf7d29932b89e532aeb5b1f78
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-18 06:09:30 +0300

    Function Expression and Declaration

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,3 +32,27 @@ console.log(appleOrangeJuice); //Re used the function
 console.log(appleJuice);
 
 //Func helps keep our code DRY(DONT REPEAT YOURSELF)
+
+/*****************************FUNCTION DECLARATIONS VERSUS FUNCTION EXPRESSIONS************** */
+
+// Function Declarations:This is a way to define a reusable block of code that performs a specific task
+// Function expression: This is a way to define functions as values as variables..
+
+/******Function Declaration */
+// function calcAgeM1(birthYear) {
+//   const age = 2037 - birthYear;
+//   return age;
+// }
+// const age = calcAgeM1(2003);
+// console.log(age, ":Years");
+
+// function calcAgeM2(birthYear) {
+//   return 2037 - birthYear;
+// }
+// console.log(calcAgeM2(1991), ":Years");
+
+/*********************************FUNCTION EXPRESSION OR ANONYMOUS FUNCTION********************* */
+// const calcAge2 = function (birtYear) {
+//   return 2037 - birtYear;
+// };
+// console.log(calcAge2(2003), "Years");

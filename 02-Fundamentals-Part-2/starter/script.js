83882152b2ea6f5aec6a35ce452c86b5912fd46b
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-18 03:49:20 +0300

    Strict Final Commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can Drive : D");
+
+// const interface = "Audio";  Resrved word
+// const private = 534; Reserved Word.
+// const if= 23; Unexpected Token 'IF'

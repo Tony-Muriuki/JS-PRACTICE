706041704fdf882390c384879d530612f120c8b2
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-02-26 13:07:09 +0300

    Continue and Break Statements

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,3 +4,40 @@
 // for (let rep = 1; rep <= 10; rep++) {
 //   console.log(`Lifting weights repetition ${rep}`);
 // }
+
+/*********************************************LOOPING ARRAYS BREAKING AND CONTINUING***************** */
+const jonasArray = [
+  "Jonas",
+  "Schedmattman",
+  2037 - 1991,
+  "teacher",
+  ["Micahel", "Peter", "Steven"],
+];
+
+// const types = [];
+
+// for (let i = 0; i < jonasArray.length; i++) {
+//   console.log(jonasArray[i]);
+//   types.push(typeof jonasArray[i]);
+// }
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+// console.log("----ONLY STRING---");
+// for (let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] !== "string") continue;
+//   console.log(jonasArray[i]);
+// }
+
+// console.log("----ONLY NUMBER---");
+// for (let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] === "number") break;
+//   console.log(jonasArray[i]);
+// }
